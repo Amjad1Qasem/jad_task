13006ef205543cd47955977be547c79f124814ee
AmjadQasem <amjadqasem07@gmail.com>
2023-11-08 10:57:19 +0300

    push commit

--- lib/screens/details/details_product.dart
+++ lib/screens/details/details_product.dart
@@ -3,21 +3,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jad/components/default_scaffold.dart';
-import 'package:jad/model/product_item_class.dart';
 
 class DetailsProduct extends StatelessWidget {
-  const DetailsProduct({super.key, required this.productName});
+  const DetailsProduct({
+    super.key,
+    required this.productName,
+  });
   final String productName;
 
   @override
   Widget build(BuildContext context) {
     return DefaultScaffold(
         appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
           title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Icon(
                 Icons.arrow_back_ios_rounded,
-                size: 18.sp,
+                size: 25.sp,
                 color: Theme.of(context).colorScheme.onPrimary,
               ),
               Text(
@@ -26,7 +30,7 @@ class DetailsProduct extends StatelessWidget {
               ),
               Icon(
                 Icons.share,
-                size: 18.sp,
+                size: 25.sp,
                 color: Theme.of(context).colorScheme.primary,
               ),
             ],
